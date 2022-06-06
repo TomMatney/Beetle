12c30778d03063bd5dc9770c7e713f4d3f6fd967
tomMatney <Matney.tommy@gmail.com>
2022-06-06 15:43:44 -0400

    I need help fixing niations

--- Beetle/Assets/_Game/Scripts/DamageDealer.cs
+++ Beetle/Assets/_Game/Scripts/DamageDealer.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class DamageDealer : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Gatherable")
         {
             collision.gameObject.GetComponent<TreeFeedback>().Tree();
+            collision.gameObject.GetComponent<PlayerFeels>().Change();
+            
         }
     }
 }
--- Beetle/Assets/_Game/WollemStartCharacter/Scripts/PlayerFeels.cs
+++ Beetle/Assets/_Game/WollemStartCharacter/Scripts/PlayerFeels.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerFeels : MonoBehaviour
 {
     public MMFeedbacks ChangeWalk;
+ 
     
     //Make a public feedbacks and make a new game object to hold the feedbacks.
     //Called using an animationEvent
